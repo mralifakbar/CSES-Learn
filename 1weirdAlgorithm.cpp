22bfce0a917b8cbf6680359d36de5d9fc2430331
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-07-07 18:39:26 +0700

    1

---
+++ 1weirdAlgorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    cout << N << " ";
+    while(N != 1)
+    {
+        if(N % 2 == 0)
+        {
+            N /= 2;
+            cout << N;
+        }
+        else
+        {
+            N = (N * 3) + 1;
+            cout << N;
+        }
+        cout << " "; 
+    }
+}
