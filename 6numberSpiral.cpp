c89f65023a9237af9e058de4ba2b7caaedc8e05b
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-07-07 21:07:17 +0700

    Create 6numberSpiral.cpp

---
+++ 6numberSpiral.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t, y, x, ans;
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> y >> x;
+        if(y == x)
+        {
+            ans = ((y - 1) * 2) + 1;
+        }
+        else if(y > x)
+        {
+            if(y % 2 == 0)
+            {
+                ans = pow(y, 2) - (x - 1);
+            }
+            else
+            {
+                ans = pow(y - 1, 2) + x;
+            }
+        }
+        else
+        {
+            if(x % 2 == 1)
+            {
+                ans = pow(x, 2) - (y - 1);
+            }
+            else
+            {
+                ans = (x * (x - 1)) - (x - 1 - y);
+            }
+        }
+        cout << ans << endl;
+    }
+}
