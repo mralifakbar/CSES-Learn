4e23ab605aec913fdfdb21b65ac0cd4b51bccddd
mralifakbar <73463443+mralifakbar@users.noreply.github.com>
2021-07-13 22:00:06 +0700

    Update 2missingNumber.cpp

--- Introductory Problems/2missingNumber.cpp
+++ Introductory Problems/2missingNumber.cpp
@@ -6,6 +6,8 @@ int main()
     long long n, sum = 0, inpt, sn;
     cin >> n;
     
+    //Jumlahkan semua inputan
+    //Kemudian kurangkan jumlah tersebut dengan deret dari n
     for(int i = 0; i < n - 1; i++)
     {
         cin >> inpt;
