b24a74a8835483686da6ebb361193020825c25a5
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-08-06 12:14:44 +0700

    Edit

--- Introductory Problems/10trailingZeros.cpp
+++ Introductory Problems/10trailingZeros.cpp
@@ -6,6 +6,8 @@ int main()
     long long int n, res = 0;
     cin >> n;
 
+    //Inti dari bilangan 0 pada hasil permutasi ialah ketika suatu bilangan bertemu dengan
+    //kelipatan 5, karena 5 dikali 2 menghasilkan 10, jadi tinggal dibagi aja bilangannya sama kelipatan 5
     for(int i = 5; i <= n; i *= 5){
         res += n / i;
     }
