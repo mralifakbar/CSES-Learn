727e2be461c4dfff1b8535c7bd2eabed12e02b42
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-08-06 12:14:13 +0700

    Update 09bitStrings.cpp

--- Introductory Problems/09bitStrings.cpp
+++ Introductory Problems/09bitStrings.cpp
@@ -6,6 +6,8 @@ int main()
     int n, ans = 1;
     cin >> n;
 
+    //Inti dari bilangan 0 pada hasil permutasi ialah ketika suatu bilangan bertemu dengan
+    //kelipatan 5, karena 5 dikali 2 menghasilkan 10, jadi tinggal dibagi aja bilangannya sama kelipatan 5
     for(int i = 1; i <= n; i++)
     {
         ans = 2 * ans % ((int)1e9 + 7); //1e9 = 10^9
